473f8830a1617ab16123b03e99b1430394476188
Pascal Brokmeier <accounts@pascalbrokmeier.de>
2018-02-16 22:44:07 +0100

    rm weather into separate project

--- weather/init.sh
+++ weather/init.sh
@@ -5,33 +5,9 @@
 /entrypoint.sh mysqld & > /data/mysqld.log
 sleep 10 #waiting for db to start up
 
-
-
-cd /weather/powertac-weather-server/scripts/
-
-#downloading the weather data
-wget http://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/uurgegevens/uurgeg_344_2001-2010.zip
-wget http://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/uurgegevens/uurgeg_344_2011-2020.zip
-unzip uurgeg_344_2001-2010.zip
-unzip uurgeg_344_2011-2020.zip
-
 cd ../
 /usr/share/tomcat7/bin/startup.sh
 echo "waiting for tomcat"
 sleep 5
+
 mvn compile tomcat7:deploy
-
-
-cd scripts/
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < powertac_weather.sql
-echo "importing data into database with python"
-python2.7 import_knmi_data.py > /data/py_import.log 2> /data/py_import_error.log
-
-#fixing file first (wrong city)
-echo "exporting data into xml file with python"
-sed -i -e 's/minneapolis/rotterdam/g' create_sim_weather.py
-python2.7 create_sim_weather.py 20100101 > /data/py_export.log 2> /data/py_export_error.log
-echo "moving exported weather to target destination"
-mv weather.xml /data
-echo "all done, goodbye"
-sleep 10000
--- weather/weather_py/util.py
+++
@@ -1,5 +0,0 @@
-def open_read_close(path) -> str:
-    f = open(path)
-    c = f.read()
-    f.close()
-    return c
