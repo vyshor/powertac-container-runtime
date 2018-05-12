f0c8a5522c99b70b01c4f9e51b30e7b0b13f7f59
pascalwhoop <accounts@pascalbrokmeier.de>
2018-05-12 12:29:00 +0200

    going environment variable based list of brokers, running competition successfully with 5 brokers

--- server/init.sh
+++ server/init.sh
@@ -3,7 +3,7 @@
 java -server -Xmx1024m -jar server.jar \
      --sim \
      --config server.properties \
-     --brokers "AgentUDE17,slytherin_v1" \
+     --brokers $BROKERS \
      --boot-data bootstrap-data.xml 
 #java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n \
 #    -server -Xmx1024m -jar target/server-war-1.5.1-SNAPSHOT-spring-boot.jar --foo=bar
