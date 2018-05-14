0d3e3fbe660b40a324404668aa2e873978324450
pascalwhoop <accounts@pascalbrokmeier.de>
2018-05-14 09:34:52 +0200

    added adapter to compose

--- server/run.sh
+++ server/run.sh
@@ -3,4 +3,4 @@
 docker rm powertac
 #instantiates a container from the just built image and exposes the two ports.
 #docker run -d --name powertac  -v powertac:/powertac -e NB_UID=1000 -e NB_GID=1000 -p 8080:8080 -p 61616:61616 pascalwhoop/powertac-server
-docker run --name powertac  -e NB_UID=1000 -e NB_GID=1000 -p 8080:8080 -p 61616:61616 pascalwhoop/powertac-server
+docker run --name powertac  -e NB_UID=1000 -e NB_GID=1000 --net host pascalwhoop/powertac-server
